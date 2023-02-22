86bba6c29469baa14be1bc4214af2379a327ecde
jeffubayi <jeffubayi@gmail.com>
2023-02-22 14:12:05 +0300

    services --> add api endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/andrecampll/api-project',
+  baseURL: 'https://my-json-server.typicode.com/jeffubayi/tokea',
 });
 
 export default api;
